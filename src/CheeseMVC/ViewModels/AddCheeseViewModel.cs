793aca0eac917d64d55fa6fe6bf711fc7b9e3121
Karen Sprong <karen.sprong@gmail.com>
2017-06-08 15:07:42 -0500

    PrepWork

---
+++ src/CheeseMVC/ViewModels/AddCheeseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.ViewModels
+{
+    public class AddCheeseViewModel
+    {
+        [Required]
+        [Display(Name = "Cheese Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "You must provide a description for your cheese")]
+        public string Description { get; set; }
+    }
+}
